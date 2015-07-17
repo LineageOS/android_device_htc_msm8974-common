b2cbcfd56da88c728b85074850290b32bec9bd9f
Thierry Strudel <tstrudel@google.com>
2015-07-16 18:30:40 -0700

    msm8974: force dex2oat not to use swap file
    Change-Id: I633f79173a6896b1243e6ca4a8f141b48b61d140

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+# Art
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
