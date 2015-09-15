f26a4163955f0b5a40dcd4741361fe15fbf549a6
Matt Mower <mowerm@gmail.com>
2015-09-15 00:34:49 -0500

    m8-common: Provide global HTC symbols via libc
    Parts of this commit originated in:
      Add HTC Logging functions to support newer HTC props.
      Richard Ross
      I70d0a6878dddf0f41d96735febda82c93496d8ec
    
    Change-Id: Ic748eb866a43336a0d6cb10c6f9394af91f667a1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,9 +43,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
 
-# Flags
-COMMON_GLOBAL_CFLAGS += -DHTCLOG
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x00000000
@@ -90,6 +87,9 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 # Includes
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Libc extensions
+BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS += libc_htc_symbols
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
