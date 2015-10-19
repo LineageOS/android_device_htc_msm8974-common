6e07d272f7477807b8d5f0782588338a3e70a72d
Steve Kondik <shade@chemlab.org>
2015-10-19 17:46:46 -0500

    msm8974: Support for native tap-to-wake
    Change-Id: I80998dbe631468be269c6f7d8e2284111b7d245d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,6 +137,7 @@ BOARD_SEPOLICY_DIRS += device/htc/msm8974-common/sepolicy
 BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     $(LOCAL_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/htc_sensorhub/sensor_hub/tap2wake"
 
 # Inherit from the proprietary version
 -include vendor/htc/msm8974-common/BoardConfigVendor.mk
