0f3fa95da49bf658f94329f5114a9324e1bc6657
Bruno Martins <bgcngm@gmail.com>
2015-10-21 13:31:30 +0100

    msm8974: Move camera defines to device trees
    Change-Id: I59288830ecfe43085b63ae0606d6972d665582dc

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -98,10 +98,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8974
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
