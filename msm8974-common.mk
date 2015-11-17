18dc2b1ee09e45045f0cc6180b9a6699289c0f2c
Bruno Martins <bgcngm@gmail.com>
2015-11-17 02:12:34 -0800

    msm8974: Copy flp.conf to build
    Change-Id: Ie2a57f1635ee9c77ee96c9a5d331113c315eb242

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -112,7 +112,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
