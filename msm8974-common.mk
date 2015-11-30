839ca1d01dcd53b4f7c37e4a43c0772684ef85b4
Steve Kondik <steve@cyngn.com>
2015-11-29 20:57:10 -0600

    msm8974: Drop all the janky proprietary stuff we don't even use
    Change-Id: I707a25fdcb0844aad1ac197e4ba8b7c662399cec

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -178,7 +178,6 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libdashplayer \
     libdivxdrmdecrypt \
     libextmedia_jni \
     libOmxAacEnc \
@@ -187,15 +186,9 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libOmxVdec \
-    libOmxVdecHevc \
     libOmxVenc \
     libOmxVidcCommon \
-    libqcmediaplayer \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
