531451c7aa798eb45f77c7763f841952ea993cf6
Bruno Martins <bgcngm@gmail.com>
2016-05-19 22:39:17 +0100

    msm8974-common: Build GPS from source
    * QC package version: LA.BF.1.1.3_rb1.13
    * Latest CAF commit for reference:
      https://us.codeaurora.org/cgit/quic/la/platform/hardware/qcom/gps/log/?h=LA.BF.1.1.3_rb1.13&id=8c74bdeda71c337baeaa3464213792ce505edc9e
    * Stock GPS configs still being used
    Change-Id: I5dc535ac86e81e6bced1d112dd5138351b13f32d

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -114,11 +114,14 @@ PRODUCT_PACKAGES += \
     Gello
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8974
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
