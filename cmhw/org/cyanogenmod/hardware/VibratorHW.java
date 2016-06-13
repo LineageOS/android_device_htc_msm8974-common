1d8741d5bbdc8a59c08e21b3aa2253c2e0dffb98
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 13:27:00 -0500

    cmhw: Use cmsdk FileUtils
    * remove a few unused classes as well
    
    Change-Id: I0e5075ce81f34a45b313b079e1cdb821fc355116

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,16 +16,14 @@
 
 package org.cyanogenmod.hardware;
 
-import java.io.File;
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 public class VibratorHW {
 
     private static String LEVEL_PATH = "/sys/devices/virtual/timed_output/vibrator/voltage_level";
 
     public static boolean isSupported() {
-        File f = new File(LEVEL_PATH);
-        return f.exists();
+        return FileUtils.isFileWritable(LEVEL_PATH);
     }
 
     public static int getMaxIntensity()  {
