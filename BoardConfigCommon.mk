5daf98c0cc320a01cb69ea8ec4db9824f6262add
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-06-16 10:21:05 -0500

    cmhw: use common VibratorHW
    Change-Id: I0841bf0bdf4e3e6f6351b5f9d2e88eb87ecfbaa3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,5 @@ TARGET_RECOVERY_DEVICE_MODULES += chargeled
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # Hardware
-BOARD_HARDWARE_CLASS := \
-    hardware/cyanogen/cmhw \
-    $(LOCAL_PATH)/cmhw
+BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/htc_sensorhub/sensor_hub/tap2wake"
