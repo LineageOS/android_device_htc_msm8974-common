db01f8f73469ed1031db39b43f76fb9642a5db16
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-06 09:32:05 -0500

    dotcase: add ability to utilize htc gestures
    * This is taken from the SlimRoms implementation of DeviceHandler
      and adapted as a service here. We'll control it through framework.
    
    Change-Id: Ifa5c193ff6501976b33952040c6449a176ae699d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,4 +132,3 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     $(LOCAL_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/htc_sensorhub/sensor_hub/tap2wake"
