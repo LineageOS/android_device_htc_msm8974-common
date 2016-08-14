e1586bf5c7e1f37fa019a136e5509038f63086c2
Ethan Chen <intervigil@gmail.com>
2016-08-14 08:27:00 +0100

    msm8974-common: GestureHandler: Add support for additional gestures
    Change-Id: I36070d611d09a2c33b9a5e14731348971624f8d5

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -11,6 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := GestureHandler
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
