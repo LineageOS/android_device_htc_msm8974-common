f88f83f3eb2125d5806b8f308decd88e6ef7b08f
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-26 07:21:06 -0500

    [DO NOT MERGE] fixes to build N
    Change-Id: I455a1f5168ae3ce4ab996c3d44c20a56d8681bdb

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -102,10 +102,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # FM Radio
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    libqcomfm_jni \
+#    qcom.fmradio
 
 # HTC logging symbols
 PRODUCT_PACKAGES += \
