330cc5aa2674e9a2337431dcf8efc16aa159a9bc
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-04 21:36:44 +0300

    msm8974-common: Cleanup init.qcom.bt.sh
    * Remove bluez support
    * Set properties in system_prop.mk
    * Bring closer to AOSP devices' script
    
    Change-Id: I6eeb69487b02070a84bc2958fb8a939a684ab915

--- system_prop.mk
+++ system_prop.mk
@@ -20,7 +20,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=smd
+    qcom.bluetooth.soc=smd \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.6 \
+    ro.bluetooth.sap=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.map=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.pbap=true \
+    ro.qualcomm.bt.hci_transport=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
