d8035def385a8c57c0e3ca69696538adc99c7fee
Ethan Chen <intervigil@gmail.com>
2016-10-30 12:44:42 +0200

    msm8974: Add GraphicBuffer shim for camera hal
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -101,6 +101,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera shim
+PRODUCT_PACKAGES += \
+    libshim_camera
+
 # FM Radio
 PRODUCT_PACKAGES += \
     FM2 \
