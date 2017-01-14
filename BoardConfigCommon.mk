000daa180900c8774af1f9aaf098dbd46e6f43c8
David Hays <dhays90@gmail.com>
2017-01-14 06:07:06 +0000

    Revert "LIBC:Compile with GCC instead of Clang to fix late-connecting ril"
    This reverts commit ac20e9b9639637bad7680642d47eb319fc53f89d.
    
    Change-Id: Ib61bd5758ff5f678e28e2eedb43d2e6dfa2e0225

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,7 +33,6 @@ TARGET_NO_BOOTLOADER := true
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
-TARGET_NEEDS_GCC_LIBC := true
 
 # Architecture
 TARGET_ARCH := arm
