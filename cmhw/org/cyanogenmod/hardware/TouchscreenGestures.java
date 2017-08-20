4806013822a9197fb952545092c59d335df89ec7
Bruno Martins <bgcngm@gmail.com>
2017-08-21 00:58:41 +0100

    msm8974-common: GestureHandler: Slot into Gestures
     * Move it from the settings dashboard to a more proper place,
       while adding again the back button into the action bar
    
    Change-Id: I90fa175d42ec47c35749870537540fbbe588ccf3

---
+++ cmhw/org/cyanogenmod/hardware/TouchscreenGestures.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *           (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+import org.cyanogenmod.internal.util.FileUtils;
+
+import cyanogenmod.hardware.TouchscreenGesture;
+
+/**
+ * Touchscreen gestures API
+ *
+ * A device may implement several touchscreen gestures for use while
+ * the display is turned off, such as drawing alphabets and shapes.
+ * These gestures can be interpreted by userspace to activate certain
+ * actions and launch certain apps, such as to skip music tracks,
+ * to turn on the flashlight, or to launch the camera app.
+ *
+ * This *should always* be supported by the hardware directly.
+ * A lot of recent touch controllers have a firmware option for this.
+ *
+ * This API provides support for enumerating the gestures
+ * supported by the touchscreen.
+ */
+public class TouchscreenGestures {
+
+    private static final String CONTROL_PATH = "/sys/devices/virtual/htc_sensorhub/sensor_hub/gesture_motion";
+
+    /**
+     * Whether device supports touchscreen gestures
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return FileUtils.isFileWritable(CONTROL_PATH);
+    }
+}
