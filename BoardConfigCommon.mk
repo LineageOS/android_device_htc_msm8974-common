3f407e2006a050d00c45a9b956b587430bca269f
Bruno Martins <bgcngm@gmail.com>
2017-08-23 15:38:59 +0100

    [DNM] msm8974: O bringup
     * Go permissive
     * Don't use ffmpeg
     * Disable breaking packages
     * Remove breaking overlays
    
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba51

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,7 @@ TARGET_CPU_VARIANT := krait
 # Kernel
 BOARD_DTBTOOL_ARGS := --dt-tag "htc,project-id = <"
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
