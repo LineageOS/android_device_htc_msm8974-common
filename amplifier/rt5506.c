6e1d2c82ce05e627c15f8f7ee4ece6ef29ef28a0
Bruno Martins <bgcngm@gmail.com>
2017-09-11 12:04:46 +0100

    msm8974-common: amplifier: Improve logging
    Change-Id: Ib6d888fde99855a01417214b66fb62c7fc6fb9df

--- amplifier/rt5506.c
+++ amplifier/rt5506.c
@@ -24,6 +24,7 @@
 
 #include <cutils/log.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
@@ -82,29 +83,34 @@ int rt5506_set_mode(audio_mode_t mode) {
     struct rt5506_config amp_config;
     int headsetohm = HEADSET_OM_UNDER_DETECT;
     int fd;
-    int ret = -1;
+    int rc = 0;
 
     /* Open the amplifier device */
     if ((fd = open(RT5506_DEVICE, O_RDWR)) < 0) {
-        ALOGE("error opening amplifier device %s", RT5506_DEVICE);
-        return -1;
+        rc = -errno;
+        ALOGE("%s: error opening amplifier device %s: %d\n",
+                __func__, RT5506_DEVICE, rc);
+        goto set_mode_err;
     }
 
     /* Get impedance of headset */
-    if (ioctl(fd, AMP_QUERY_OM, &headsetohm) < 0)
-        ALOGE("error querying headset impedance");
+    if (ioctl(fd, AMP_QUERY_OM, &headsetohm) < 0) {
+        rc = -errno;
+        ALOGE("%s: error querying headset impedance: %d\n", __func__, rc);
+        goto set_mode_err;
+    }
 
     switch(mode) {
         default:
         case AUDIO_MODE_NORMAL:
             /* For headsets with a impedance between 128ohm and 1000ohm */
             if (headsetohm >= HEADSET_128OM && headsetohm <= HEADSET_1KOM) {
-                ALOGI("Mode: Playback 128");
+                ALOGI("%s: Mode: Playback 128\n", __func__);
                 amp_config.reg_len = sizeof(rt5506_playback_128_data) / sizeof(struct rt5506_reg_data);
                 memcpy(&amp_config.reg, rt5506_playback_128_data, sizeof(rt5506_playback_128_data));
                 amp_data.out_mode = PLAYBACK_MODE_PLAYBACK128OH;
             } else {
-                ALOGI("Mode: Playback");
+                ALOGI("%s: Mode: Playback\n", __func__);
                 amp_config.reg_len = sizeof(rt5506_playback_data) / sizeof(struct rt5506_reg_data);
                 memcpy(&amp_config.reg, rt5506_playback_data, sizeof(rt5506_playback_data));
                 amp_data.out_mode = PLAYBACK_MODE_PLAYBACK;
@@ -112,7 +118,7 @@ int rt5506_set_mode(audio_mode_t mode) {
             amp_data.config = amp_config;
             break;
         case AUDIO_MODE_RINGTONE:
-            ALOGI("Mode: Ring");
+            ALOGI("%s: Mode: Ring\n", __func__);
             amp_config.reg_len = sizeof(rt5506_ring_data) / sizeof(struct rt5506_reg_data);
             memcpy(&amp_config.reg, rt5506_ring_data, sizeof(rt5506_ring_data));
             amp_data.out_mode = PLAYBACK_MODE_RING;
@@ -120,7 +126,7 @@ int rt5506_set_mode(audio_mode_t mode) {
             break;
         case AUDIO_MODE_IN_CALL:
         case AUDIO_MODE_IN_COMMUNICATION:
-            ALOGI("Mode: Voice");
+            ALOGI("%s: Mode: Voice\n", __func__);
             amp_config.reg_len = sizeof(rt5506_voice_data) / sizeof(struct rt5506_reg_data);
             memcpy(&amp_config.reg, rt5506_voice_data, sizeof(rt5506_voice_data));
             amp_data.out_mode = PLAYBACK_MODE_VOICE;
@@ -129,11 +135,14 @@ int rt5506_set_mode(audio_mode_t mode) {
     }
 
     /* Set the selected config */
-    if ((ret = ioctl(fd, AMP_SET_CONFIG, &amp_data)) != 0) {
-        ALOGE("ioctl %d failed. ret = %d", AMP_SET_CONFIG, ret);
+    if ((rc = ioctl(fd, AMP_SET_CONFIG, &amp_data)) != 0) {
+        rc = -errno;
+        ALOGE("%s: ioctl AMP_SET_CONFIG failed, ret = %d\n", __func__, rc);
+        goto set_mode_err;
     }
 
+set_mode_err:
     close(fd);
 
-    return ret;
+    return rc;
 }
