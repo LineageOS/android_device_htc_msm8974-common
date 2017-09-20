697cf7b6384002e1995249fc4fc8c005b93eb16f
Bruno Martins <bgcngm@gmail.com>
2017-09-20 12:30:55 +0100

    msm8974-common: Take local sepolicies into account
     * Still empty, but it's just to be prepared for
       the upcoming changes.
    
    Change-Id: I883445a805f82411418d29af20dc688bebb0e91a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,6 +117,7 @@ TARGET_USE_SDCLANG := true
 # SELinux
 include device/qcom/sepolicy/legacy-sepolicy.mk
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
