2c60173edac84ddad7c42778bd226bb7e2d26dc1
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 18:59:09 +0200

    msm8974-common: Move sec_config to vendor
    Change-Id: Ie5f7c8f96243a4ed354f87d78ede9176a46b44be

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 
 # IPC router config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # Input device config
 PRODUCT_COPY_FILES += \
