7033c23c987046424b88fa8988b48c0be31aae09
Paul Keith <javelinanddart@gmail.com>
2018-02-19 18:21:29 +0100

    msm8974-common: Use sdFAT for exFAT
    Change-Id: I660c5b3008636a651a2812fe1739ff8a2ea845cb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,7 +97,7 @@ ifeq ($(HOST_OS),linux)
 endif
 
 # Extended filesystem support
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
