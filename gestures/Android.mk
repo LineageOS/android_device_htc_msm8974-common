76326b538d30b635267c3496a2bc44b69635ac31
Anton Hansson <hansson@google.com>
2018-02-21 14:11:44 +0000

    msm8974-common: gestures: Set LOCAL_SDK_VERSION to current
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Iabb0556dc1c80c7fc7f6c76d61d5e441b03cdce0

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -23,6 +23,7 @@ LOCAL_RESOURCE_DIR := \
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := GestureHandler
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
