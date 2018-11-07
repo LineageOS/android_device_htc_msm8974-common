d9ca562d0ede716d6136e226194538061abce358
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    msm8974-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -26,8 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
