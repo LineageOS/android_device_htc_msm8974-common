ea39eb3feb3e59fb572e8e68fb80e029c18ea2e6
Bruno Martins <bgcngm@gmail.com>
2019-09-15 10:33:25 +0100

    [DNM] msm8974-common: Common bring up changes
    Change-Id: Icd84daffd9b84107e8ee0f4bfd5da2c7842af991

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,7 @@ TARGET_USES_64_BIT_BINDER := true
 # Kernel
 BOARD_DTBTOOL_ARGS := --dt-tag "htc,project-id = <"
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
@@ -140,8 +141,9 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_USE_SDCLANG := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/tmp
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
