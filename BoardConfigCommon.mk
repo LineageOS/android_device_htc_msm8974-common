LineageOS/android_device_samsung_msm8974-common

msm8974-common: Use shared blobs from vendor/
Change-Id: I8f8675b9fd1950cd7c64538f160e73514209be19

BoardConfigCommon.mk
@@ -57,3 +57,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8974
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
+
+# inherit from the proprietary version
+-include vendor/samsung/msm8974-common/BoardConfigVendor.mk

LineageOS/android_device_htc_msm8974-common

BoardConfigCommon.mk
@@ -172,3 +172,6 @@ BOOTLOADER_MESSAGE_OFFSET := 2048
 TARGET_RECOVERY_DEVICE_DIRS += $(PLATFORM_PATH)
 TARGET_RECOVERY_DEVICE_MODULES += chargeled
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit from the proprietary version
+-include vendor/htc/msm8974-common/BoardConfigVendor.mk