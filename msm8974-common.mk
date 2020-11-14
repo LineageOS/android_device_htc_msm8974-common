8e72f9981fae19dc71cf21c76c50ac198b60fecf
Arne Coucheron <arco68@gmail.com>
2020-11-14 05:14:09 +0100

    msm8974-common: Build vendor variant of libprotobuf-cpp-lite.so
    We need it for our prebuilt drm library.
    
    Change-Id: Ie2bfc0c5a65451d54d44d820af0d985cbb9a1a6a

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    libprotobuf-cpp-lite-vendorcompat
 
 # FM Radio
 PRODUCT_PACKAGES += \
