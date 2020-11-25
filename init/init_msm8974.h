05af5cfb10f4fccc0cbdc762ef9831649b0f675c
Bruno Martins <bgcngm@gmail.com>
2020-11-25 14:37:07 +0000

    msm8974-common: Create common libinit to hold helper functions
    This helps deduplicating code in the device-specific init modules.
    
    Change-Id: Id2e2285e93c341644a996d14d7b4769bbb07dc2d

---
+++ init/init_msm8974.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_ro_build_fingerprint_prop(char const value[]);
+void set_ro_product_prop(char const prop[], char const value[]);
