LineageOS/android_device_samsung_msm8974-common

msm8974-common: Don't let builds complete without vendor tree
* There is zero reason to ever build without blobs. We've even seen
  this situation with official builds from our servers. It's always
  better for a build to fail than it is for it to produce something
  that has no chance at working.

Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2019,2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -100,4 +100,4 @@ TARGET_RECOVERY_DEVICE_DIRS += device/samsung/msm8974-common
 BOARD_USES_QC_TIME_SERVICES := true
 
 # inherit from the proprietary version
--include vendor/samsung/msm8974-common/BoardConfigVendor.mk
+include vendor/samsung/msm8974-common/BoardConfigVendor.mk

LineageOS/android_device_htc_msm8974-common

BoardConfigCommon.mk
@@ -177,4 +177,4 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 TARGET_VNDK_USE_CORE_VARIANT := true
 
 # Inherit from the proprietary version
--include vendor/htc/msm8974-common/BoardConfigVendor.mk
+include vendor/htc/msm8974-common/BoardConfigVendor.mk
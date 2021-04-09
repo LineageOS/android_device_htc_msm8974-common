4455d04322cb406018f76f4dd548f7ff4a4ae713
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    msm8974-common: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -22,7 +22,7 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/htc/msm8974-common/msm8974-common-vendor.mk)
+$(call inherit-product, vendor/htc/msm8974-common/msm8974-common-vendor.mk)
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
