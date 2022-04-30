0fd466f1b2455e1467904c45db7e81ab48912ef7
Nolen Johnson <johnsonnolen@gmail.com>
2022-04-29 22:49:46 -0400

    msm8974-common: chargeled: Resolve -Wwritable-strings warnings
    Change-Id: I46c800b4976a35daf3fd3b60622e784cbee36378

--- chargeled/chargeled.cpp
+++ chargeled/chargeled.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2014 The CyanogenMod Project
- *               2020 The LineageOS Project
+ *               2020,2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@
 static int uevent_fd;
 
 struct sysfs_string_enum_map {
-    char* str;
+    const char *str;
     int val;
 } battery_status_map[] = {
     { "Unknown", BATTERY_STATUS_UNKNOWN },
